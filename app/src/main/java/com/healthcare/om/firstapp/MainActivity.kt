520dd9cb7329d83ba9453342a9639717db55da58
Biswa <>
2018-10-25 18:16:27 +0530

    Toast Message

--- app/src/main/java/com/healthcare/om/firstapp/MainActivity.kt
+++ app/src/main/java/com/healthcare/om/firstapp/MainActivity.kt
@@ -2,11 +2,17 @@ package com.healthcare.om.firstapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var container = "Apple"
+        Toast.makeText(MainActivity@this,container,Toast.LENGTH_SHORT).show()
+
+
     }
 }
